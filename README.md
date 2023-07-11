a19d48c3d4bbf00b12dfeb1e8d75dc0178eb2921
stoswebsolutions <stoswebsolutions@gmail.com>
2023-07-11 10:59:42 +0530

    mongoose database code

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ step 1 npm init
 step 2 npm install express mongoose nodemon dotenv
 step 3 start file create mgb.js
 step 4 env file and server basic start code
+step 5 mongoose database connected
--- mgb.js
+++ mgb.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Database connected...'))
+    .catch((err) => { console.log(err) })
 
 
 app.listen(process.env.PORT, () => {
